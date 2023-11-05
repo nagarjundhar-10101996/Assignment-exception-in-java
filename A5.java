73b3c12110156fc273c911a9873de8e0346b7b2c
nagarjundhar-10101996 <142318278+nagarjundhar-10101996@users.noreply.github.com>
2023-11-05 15:31:51 +0530

    Update A5.java
    Duplicate value check entirely in array then print if found then exception or simple not found

--- A5.java
+++ A5.java
@@ -1,81 +1,35 @@
 import java.util.Scanner;
-
-
-
 class CustomException extends Exception{
-
 	private int n;
-
-	CustomException(int n){
-
-		this.n=n;
-
-	}
-
-	public int getN() {
-
-		return n;
-
-	}
-
-	public void setN(int n) {
-
-		this.n = n;
-
-	}
-
-	
-
+	CustomException(int n){this.n=n;}
+	public int getN(){return n;}
+	public void setN(int n) {this.n = n;}
 }
-
-
-
 public class A5 {
-
 	public static void main(String[] args) throws CustomException {
-
-		// TODO Auto-generated method stub
-
 		int[] a=new int[100001];
-
 		int num;
-
 		Scanner sc=new Scanner(System.in);
-
-		System.out.println("Please enter 10 integers ranging from 1 to 100000");
-
-	
-
+		System.out.println("Please enter 5 integers ranging from 1 to 100000");
 			int i=0;
-
 			try {
-
-				while(i<10) {
+				while(i<5) {
                     num=sc.nextInt();
-					a[i]=num;
-
-					if(a[i]==a[i+1]) {
-
-						throw new CustomException(num);
-
-					}
-
-					else {
-
-						a[i]=1;
-						i++;
-					}
-
+					a[i++]=num;	
 				}
-
+				int j = i;
+				while (i > 0) {
+					--i;
+					while (j > 0 ) {
+						if (a[i] == a[j]) {
+							throw new CustomException(a[j]);
+						}
+						--j;	
+					}
+				}
 			System.out.println("No duplicate numbers!");
-
 		}catch(CustomException e) {
-
 			System.out.println("Error: Duplicate number found: "+e.getN());
-
 		}
-
 	}
-
 }
